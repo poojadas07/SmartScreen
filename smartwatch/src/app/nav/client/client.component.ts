bc0517dd78f51b1b547f9804a4ae0d40101e3623
poojadas07 <poojadas04kv@gmail.com>
2021-05-24 12:04:12 +0530

    client and department page created

--- smartwatch/src/app/nav/client/client.component.ts
+++ smartwatch/src/app/nav/client/client.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ApiService } from 'src/app/service/api.service';
+import { ClientAddComponent } from './client-add/client-add.component';
 
 @Component({
   selector: 'app-client',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientComponent implements OnInit {
 
-  constructor() { }
+  clients: any;
+  searchvalue: string;
 
-  ngOnInit(): void {
+  constructor(private apiService: ApiService,
+    public dialog: MatDialog) { }
+
+  ngOnInit() {
+
+    this.apiService.fetchAllCountries().subscribe((res) => {
+        this.clients = res;
+    });
+  }
+
+  search(searchvalue) : void {
+    alert(searchvalue)
+  }
+
+  editRow(){
+    console.log('hhh')
+  }
+
+  deleteRow(){
+    console.log('hhh')
+  }
+
+  openDialog() {
+    this.dialog.open(ClientAddComponent);
   }
 
 }
--- smartwatch/src/app/nav/department/department.component.ts
+++ smartwatch/src/app/nav/department/department.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ApiService } from 'src/app/service/api.service';
+import { DepartmentAddComponent } from './department-add/department-add.component';
 
 @Component({
   selector: 'app-department',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentComponent implements OnInit {
 
-  constructor() { }
+  departments: any;
+  searchvalue: string;
 
-  ngOnInit(): void {
+  constructor(private apiService: ApiService,
+    public dialog: MatDialog) { }
+
+  ngOnInit() {
+
+    this.apiService.fetchAllCountries().subscribe((res) => {
+        this.departments = res;
+    });
+  }
+
+  search(searchvalue) : void {
+    alert(searchvalue)
+  }
+
+  editRow(){
+    console.log('hhh')
+  }
+
+  deleteRow(){
+    console.log('hhh')
+  }
+
+  openDialog() {
+    this.dialog.open(DepartmentAddComponent);
   }
 
 }
