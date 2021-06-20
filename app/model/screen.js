33b409f240e381cd9acb29785a9d84556a06d2d4
poojadas07 <poojadas04kv@gmail.com>
2021-06-20 17:14:17 +0530

    Screen name uniqueness changes

--- app/model/screen.js
+++ app/model/screen.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Screen = new mongoose.Schema({
     name: {
         type: String,
-        unique: true
+        unique: false
     },
     rows: {
         type: Number,      
